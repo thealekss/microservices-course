133ac782f94ec26ab9ffc2dd58f583f456edd9f2
Alex <alex@mail.ru>
2021-12-24 13:49:07 +0300

    Add module2

---
+++ src/main/java/com/example/apptest/aspect/LoggingAspect.java
@@ -0,0 +1,23 @@
+package com.example.apptest.aspect;
+
+import com.example.apptest.model.Greeting;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ReflectionUtils;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    private Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+
+    @AfterReturning(pointcut = "execution(* com.example.apptest.controller.GreetingController.*(..))", returning = "result")
+    void logIncomeOutcomeParams(JoinPoint joinPoint, Object result) {
+        logger.info("Вызван метод: {}, параметры: {}", joinPoint.getSignature().getName(),
+                joinPoint.getArgs());
+    }
+}
