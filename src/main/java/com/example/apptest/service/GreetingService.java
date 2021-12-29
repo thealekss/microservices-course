8fee13bfd079ed8072957c8741f15767fa6708c9
Alex <alex@mail.ru>
2021-12-29 08:46:30 +0300

    Add module6

--- src/main/java/com/example/apptest/service/GreetingService.java
+++ src/main/java/com/example/apptest/service/GreetingService.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -35,6 +36,7 @@ public class GreetingService {
         return greetingRepository.findById(id);
     }
 
+    @Transactional
     public void replaceGreeting(Greeting greeting, long id) {
         greeting.setId(id);
         greetingRepository.save(greeting);
